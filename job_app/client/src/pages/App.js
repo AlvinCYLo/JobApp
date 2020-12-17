d0fa2eda80762e0d6937229cb05e71c4c99c37f1
AlvinCYLo <alvin.cy.lo@gmail.com>
2020-12-17 15:42:10 -0800

    Updated App with sideNavBar and added public folder from CRA

--- job_app/client/src/pages/App.js
+++ job_app/client/src/pages/App.js
@@ -1,19 +1,163 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ContactsIcon from '@material-ui/icons/Contacts';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import WorkIcon from '@material-ui/icons/Work';
 
 import Applied from './Applied/Applied';
 import Jobs from './Jobs/Jobs';
 import Info from './Info/Info';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+  // const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/jobs" component={Jobs} />
-        <Route path="/applied" component={Applied} />
-        <Route path="/info" component={Info} />
-      </Switch>
-    </Router>
+    <>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            {open ? (
+              <IconButton onClick={handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            ) : (
+              <IconButton onClick={handleDrawerOpen}>
+                <ChevronRightIcon />
+              </IconButton>
+            )}
+          </div>
+          <Divider />
+          <List>
+            <ListItem button key="Jobs">
+              <ListItemIcon>
+                <WorkIcon />
+              </ListItemIcon>
+              <ListItemText primary="Jobs" />
+            </ListItem>
+            <ListItem button key="Applied">
+              <ListItemIcon>
+                <CheckCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Applied" />
+            </ListItem>
+            <ListItem button key="Info">
+              <ListItemIcon>
+                <ContactsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Info" />
+            </ListItem>
+          </List>
+        </Drawer>
+
+        <Router history={createBrowserHistory}>
+          <Switch>
+            <Route path="/jobs" component={Jobs} />
+            <Route path="/applied" component={Applied} />
+            <Route path="/info" component={Info} />
+            <Redirect from="/" to="/jobs" />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 };
 
